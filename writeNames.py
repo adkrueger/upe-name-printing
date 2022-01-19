10130fbc9722df109f478f78b8fa57923f6bd088
adkrueger <adkrueger@wpi.edu>
2022-01-19 14:58:54 -0500

    fixing off by 1 error

--- writeNames.py
+++ writeNames.py
@@ -6,7 +6,7 @@ names = pd.read_excel(filename, sheet_name="lastNameOrder")
 induction_number = 66
 
 with open("sorted_names.txt", "w") as write_file:
-    for i in range(1, len(names["Last Name"])):
+    for i in range(0, len(names["Last Name"])):
         write_file.write(f"{induction_number},\"{names['First Name'][i]} {names['Last Name'][i]}\",false\n")
 
 print("done!")
